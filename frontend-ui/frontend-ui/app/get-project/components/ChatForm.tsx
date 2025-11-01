bb593fd25bbb5b55a26eeb6aec48d47802f04c57
Sumanvitha <sumanvitha.eadala89@gmail.com>
2025-11-01 21:16:32 +0530

    fixed prop issue

--- frontend-ui/frontend-ui/app/get-project/components/ChatForm.tsx
+++ frontend-ui/frontend-ui/app/get-project/components/ChatForm.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Send } from "lucide-react";
-import UserHistory from "@/app/user-history/page";
+import UserHistory from "@/app/user-history/components/UserHistory";
 import LogOutPage from "@/app/(auth)/logout/LogOutPage";
 
 type Message = {
