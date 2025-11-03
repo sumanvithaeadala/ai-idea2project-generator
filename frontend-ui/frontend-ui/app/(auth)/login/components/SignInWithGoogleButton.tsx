f6929bc107db280f64cc702d141485e10c4ae513
Sumanvitha <sumanvitha.eadala89@gmail.com>
2025-11-03 14:08:09 +0530

    updated signin with google functionality

--- frontend-ui/frontend-ui/app/(auth)/login/components/SignInWithGoogleButton.tsx
+++ frontend-ui/frontend-ui/app/(auth)/login/components/SignInWithGoogleButton.tsx
@@ -1,17 +1,36 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { signInWithGoogle } from "@/lib/auth-actions";
-import React from "react";
+import { createClient } from "@/utils/supabase/client";
+
 
 const SignInWithGoogleButton = () => {
+  const handleGoogleSignIn = async () => {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/get-project`,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+
+    if (error) {
+      console.error("Google Sign-In Error:", error);
+    } else {
+      window.location.href = data.url;
+    }
+  };
+
   return (
     <Button
       type="button"
       variant="outline"
       className="w-full"
-      onClick={() => {
-        signInWithGoogle();
-      }}
+      onClick={handleGoogleSignIn}
     >
       Login with Google
     </Button>
